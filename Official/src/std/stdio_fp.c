dd3a27bf128d51ff20e096db50e98343f1c3ad3f
daizhirui <daizhirui@hotmail.com>
2018-06-04 11:37:57 +0200

    Now stdio_fp supports outputting 5 effective fraction digits.

--- Official/src/std/stdio_fp.c
+++ Official/src/std/stdio_fp.c
@@ -21,15 +21,38 @@ char* ftoa(float a_fp)
 {
     static char buf[33];
     _memset(buf,33,0);
-    long integer_part = fp_float32_to_int32(a_fp);
-    itoa(integer_part, 10);
-    _strcat(buf,__convertBuffer);
-    _strcat(buf, ".");               //append decimal point
-    a_fp = abs(a_fp);
-    int frac_part = (a_fp - fp_int32_to_float32(integer_part)) * 1000;  //subtract to get the decimals, and multiply by 1000
-    itoa(frac_part, 10);                     //convert to a second string
-    _strcat(buf, __convertBuffer);          //and append to the first
-    return buf;
+    if (a_fp < 1.0f) {
+        unsigned char pos = 0;
+        if ((*(unsigned long*)&(a_fp))&0x80000000) { // check sign
+            buf[0] = '-';
+            buf[1] = '0';
+            buf[2] = '.';
+            pos = 3;
+        } else {
+            buf[0] = '0';
+            buf[1] = '.';
+            pos = 2;
+        }
+
+        while((a_fp *= 10) < 1) {   // find a none 0 beginning
+            buf[pos++] = '0';
+        }
+        a_fp *= 10000;
+        int frac_part = fp_float32_to_int32(a_fp);
+        itoa(frac_part, 10);
+        _strcat(buf, __convertBuffer);
+        return buf;
+    } else {
+        long integer_part = fp_float32_to_int32(a_fp);
+        itoa(integer_part, 10);
+        _strcat(buf,__convertBuffer);
+        _strcat(buf, ".");               //append decimal point
+        a_fp = abs(a_fp);
+        int frac_part = (a_fp - fp_int32_to_float32(integer_part)) * 100000;  //subtract to get the decimals, and multiply by 1000
+        itoa(frac_part, 10);                     //convert to a second string
+        _strcat(buf, __convertBuffer);          //and append to the first
+        return buf;
+    }
 }
 
 void _printf(char* buf, const char *format, va_list ap)
